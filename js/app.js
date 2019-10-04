24e9e2ba7a062f095261f06344880931c1ad70f5
Neil <neiljessup@gmail.com>
2019-10-04 17:28:50 -0500

    buttons created, user class created, game object create

---
+++ js/app.js
@@ -0,0 +1,15 @@
+class User {
+  constructor(name){
+    this.red = null,
+    this.blue = null,
+    this.green = null,
+    this.points = null,
+    this.buildins = []
+
+  }
+}
+const user = new User(name);
+const game = {
+  rounds: null,
+  turns: null,
+}
