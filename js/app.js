685176b3aca99b8f95c2d9fdced8aea725cda2b7
Neil <neiljessup@gmail.com>
2019-10-06 16:01:29 -0500

    Turn sequence for one player built and tested, still needs endstate fixed

--- js/app.js
+++ js/app.js
@@ -17,14 +17,15 @@ class User {
 //   }
 // }
 const game = {
-  rounds: null,
+  rounds: 1,
   turns: null,
   player: null,
+  turnCounter: 2,
+  sum: null,
 
   start(){
     const user = new User($('#inputBox').val());
     this.player = user;
-
 
   },
   // generates and allocates resource selections
@@ -42,58 +43,103 @@ const game = {
   },
   addRandom(){
     //this.player.red += 3 ;
-    let randomRes = Math.floor(Math.random()*2) + 1;
+    let randomRes = Math.floor(Math.random()*3);
       console.log(randomRes, ":randon int");
-    if (randomRes == 1) {
+    if (randomRes == 0) {
       this.addRed();
-    } else if (randomRes == 2) {
+    } else if (randomRes == 1) {
       this.addBlue();
     } else {
       this.addGreen();
     }
-    console.log(this.player.red, " :red");
-    console.log(this.player.blue, " :blue");
-    console.log(this.player.green, " :green");
   },
   buyBasicBuilding(){
     //const building = new Structure (basic);
+    const $p1Points = $('#p1Points');
+    if (this.player.red > 0 && this.player.blue > 0 &&
+      this.player.green > 0) {
 
-    this.player.red -= 1;
-    this.player.blue -= 1;
-    this.player.green -= 1;
-    this.player.buildings = this.player.buildings.push(1);
-    console.log(game.player.buildings, " :in function");
+        this.player.red -= 1;
+        this.player.blue -= 1;
+        this.player.green -= 1;
+        this.player.buildings.push(1);
+        this.player.points++;
+        $p1Points.text(`Player 1 points: ${game.player.points}`)
+        console.log(game.player.buildings, " :in function");
+    } else {
+      console.log('not enough resources');
+    }
+  },
+  sumPointsArray(){
+  this.player.buildings.reduce(reducer);
 
+
+  },
+  endTurn(){
+    const $rounds = $('#rounds');
+    if (this.turnCounter === 0) {
+      this.turnCounter += 2;
+      game.rounds++;
+      $rounds.text(`Round: ${game.rounds}`);
+    }
   }
 }
 
 $('#red').on('click', () => {
-  game.addRed();
-  game.addRed();
+  if (game.turnCounter > 0) {
+    game.addRed();
+    game.addRed();
+    game.turnCounter--;
+  } else {
+    console.log('No more actions available');
+  }
 });
 
 $('#blue').on('click', () => {
-  game.addBlue();
-  game.addBlue();
-
+  if (game.turnCounter > 0) {
+    game.addBlue();
+    game.addBlue();
+    game.turnCounter--;
+  } else {
+    console.log('No more actions available');
+  }
 });
 
 $('#green').on('click', () => {
-  game.addGreen();
-  game.addGreen();
-
+  if (game.turnCounter > 0) {
+    game.addGreen();
+    game.addGreen();
+    game.turnCounter--;
+  } else {
+    console.log('No more actions available');
+  }
 });
 
 $('#random').on('click', () => {
-  game.addRandom();
-  game.addRandom();
-  game.addRandom();
-
+  if (game.turnCounter > 0) {
+    game.addRandom();
+    game.addRandom();
+    game.addRandom();
+    game.turnCounter--;
+  } else {
+    console.log('No more actions available');
+  }
+  console.log(game.player.red, " :red");
+  console.log(game.player.blue, " :blue");
+  console.log(game.player.green, " :green");
 });
 
 $('#purchase').on('click', () => {
   game.buyBasicBuilding();
+
   console.log(game.player.buildings,' :event');
+
+});
+
+$('#endTurn').on('click', () => {
+  game.endTurn();
+
+
 
 });
 $('form').on('submit', (e) => {
