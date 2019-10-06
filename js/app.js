084612161cf6b8c40360db3ad5873b6cef6c1484
Neil <neiljessup@gmail.com>
2019-10-05 19:50:16 -0500

    added buttons, event listeners, and simple functions for allocation of resources

--- js/app.js
+++ js/app.js
@@ -8,8 +8,46 @@ class User {
 
   }
 }
-const user = new User(name);
+
 const game = {
   rounds: null,
   turns: null,
+  player: null,
+
+  start(){
+    const user = new User($('#inputBox').val());
+    this.player = user;
+
+
+  },
+  // generates and allocates resource selections
+  addRed(){
+    this.player.red += 2 ;
+
+  },
+  addBlue(){
+    this.player.blue += 2 ;
+
+  },
+  addGreen(){
+    this.player.green += 2 ;
+
+  },
+  addRandom(){
+    //this.player.red += 3 ;
+
+  },
 }
+
+$('#red').on('click', () => {
+  game.player.addRed();
+
+});
+$('#blue').on('click', () => {
+  game.player.addBlue();
+
+});
+$('#green').on('click', () => {
+  game.player.addGreen();
+
+});
