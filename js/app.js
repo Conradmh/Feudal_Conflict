8d495934f1e8bac9d57c3e14c5ece9bd5b2a8612
Neil <neiljessup@gmail.com>
2019-10-09 13:50:38 -0500

    2 player resource generation and display working, no buy functionaility yet

--- js/app.js
+++ js/app.js
@@ -1,55 +1,104 @@
 class User {
-  constructor(name){
+  constructor(id, name){
     this.name = name,
+    this.id = id,
+    this.actions = 1,
+    //this.myTurn = false,
     this.red = 0,
     this.blue = 0,
     this.green = 0,
     this.points = 0,
     this.buildings = []
-
   }
+  addRed(){
+    this.red += 2 ;
+    console.log(this.red, " :addRed function");
+    this.actions--;
+    this.showStats();
+  }
+  addBlue(){
+    this.blue += 2 ;
+    console.log(this.blue, ' :addBlue function');
+    this.actions--;
+    this.showStats();
+  }
+  addGreen(){
+    this.green += 2 ;
+    console.log(this.green, " :addGreenfunction");
+    this.actions--;
+    this.showStats();
+  }
+  addRandom(){
+
+    let randomRes = Math.floor(Math.random()*3);
+    console.log(randomRes, ":randon int");
+    if (randomRes == 0) {
+      this.red += 1;
+    } else if (randomRes == 1) {
+      this.blue += 1;
+    } else {
+      this.green += 1;
+    }
+
+    this.showStats();
+  }
+  // buyBasicBuilding(){
+  //   //const building = new Structure (basic);
+  //
+  //   //reformat this to put the 'this.' in the player class and call it
+  //   const $p1Points = $('#p1Points');
+  //   const $buyButton = $(`#buy`);
+  //   if (this.player.red > 0 && this.player.blue > 0 &&
+  //     this.player.green > 0) {
+  //
+  //       this.player.red -= 1;
+  //       this.player.blue -= 1;
+  //       this.player.green -= 1;
+  //       this.player.buildings.push(1);
+  //       this.player.points++;
+  //       $p1Points.text(`Player 1 points: ${game.player.points}`)
+  //       console.log(game.player.buildings, " :in function");
+  //       this.player.showStats();
+  //     } else {
+  //       $buyButton.prop(`disabled`, true);
+  //       console.log('not enough resources');
+  //     }
+  //   },
 
   showStats() {
-    const $p1Name = $(`#p1Name`);
-    const $p1Red = $(`#p1Red`);
-    const $p1Blue = $(`#p1Blue`);
-    const $p1Green = $(`#p1Green`);
-    const $p1Points = $(`#p1Points`);
+   const $pName = $(`#p${this.id}Name`);
+   const $pRed = $(`#p${this.id}Red`);
+   const $pBlue = $(`#p${this.id}Blue`);
+   const $pGreen = $(`#p${this.id}Green`);
+   const $pPoints = $(`#p${this.id}Points`);
+   const $pActions = $(`#p${this.id}Actions`);
 
-    $p1Name.text(`${game.player.name}`);
-    $p1Red.text(`Red: ${game.player.red}`);
-    $p1Blue.text(`Blue: ${game.player.blue}`);
-    $p1Green.text(`Green: ${game.player.green}`);
-    $p1Points.text(`Score: ${game.player.points}`);
+   $pName.text(`${this.name}`);
+   $pRed.text(`Red: ${this.red}`);
+   $pBlue.text(`Blue: ${this.blue}`);
+   $pGreen.text(`Green: ${this.green}`);
+   $pPoints.text(`Score: ${this.points}`);
+   $pActions.text(`Actions: ${this.actions}`);
 
   }
 
 };
-// class Structure {
-//   constructor(name){
-//     this.name = name,
-//     this.points = 0,
-//   }
-// }
 const game = {
   rounds: 1,
-  turns: null,
   player: [], //array of players for multi
-  turnCounter: 2,
+  turnCounter: 4, //will  need to be 2x the number of names in array
   sum: null,
-//players: null, number of players as a property
-  start(){
-  //   this.createPlayer();
-  //   this.createPlayer();
-     $('form').hide();
-  //   //user.showStats();
-  //
-  },
+  currentPlayer: 0,
+
+  // start(){
+  //    $('form').hide();
+  // },
 
   // consider writing one function that takes an input of
   // the event listener (console.log([e.target]);)
   createPlayer(){
-    const user = new User($('#inputBox').val());
+    const idNumber = this.player.length + 1;
+    const user = new User(idNumber, $('#inputBox').val());
     this.player.push(user);
     console.log(this.player);
     user.showStats();
@@ -58,70 +107,49 @@ const game = {
 
     // change attr of placeholder with interpolation of array index
   },
-  addRed(){
-    this.player.red += 1 ;
-    console.log(this.player.red, " :addRed function");
-    this.player.showStats();
-
-  },
-  addBlue(){
-    this.player.blue += 1 ;
-    console.log(this.player.blue, ' :addBlue function');
-    this.player.showStats();
-
-  },
-  addGreen(){
-    this.player.green += 1 ;
-    console.log(this.player.green, " :addGreenfunction");
-    this.player.showStats();
-  },
-  addRandom(){
-    //this.player.red += 3 ;
-    let randomRes = Math.floor(Math.random()*3);
-      console.log(randomRes, ":randon int");
-    if (randomRes == 0) {
-      this.addRed();
-    } else if (randomRes == 1) {
-      this.addBlue();
-    } else {
-      this.addGreen();
-    }
-    this.player.showStats();
-  },
-  buyBasicBuilding(){
-    //const building = new Structure (basic);
-
-    //reformat this to put the 'this.' in the player class and call it
-    const $p1Points = $('#p1Points');
-    const $buyButton = $(`#buy`);
-    if (this.player.red > 0 && this.player.blue > 0 &&
-      this.player.green > 0) {
-
-        this.player.red -= 1;
-        this.player.blue -= 1;
-        this.player.green -= 1;
-        this.player.buildings.push(1);
-        this.player.points++;
-        $p1Points.text(`Player 1 points: ${game.player.points}`)
-        console.log(game.player.buildings, " :in function");
-        this.player.showStats();
-    } else {
-      $buyButton.prop(`disabled`, true);
-      console.log('not enough resources');
-    }
-  },
-  sumPointsArray(){
-  this.player.buildings.reduce(reducer);
+  currentTurn(){
+    const $playersTurn = $('#turn');
+    $playersTurn.text(`| ${this.player[game.currentPlayer].name}'s  Turn`);
 
 
   },
+  currentRound(){
+    const $gameRound = $('#rounds');
+    $gameRound.text(`Round: ${this.rounds} |`);
+
+
+  },
+  // curPlayer(){
+  //   this.currentPlayer = this.player[this.turnCounter % 2];
+  // },
+  // sumPointsArray(){
+  // this.player.buildings.reduce(reducer);
+  //
+  // },
   endTurn(){
-    const $rounds = $('#rounds');
-    if (this.turnCounter === 0) {
-      this.turnCounter += 2;
-      game.rounds++;
-      $rounds.text(`Round: ${game.rounds}`);
-    }
+    // if currentPlayer is array.length-1
+      // currentPlayer = 0
+    // else
+      // currentPlayer++
+
+      if (this.player[game.currentPlayer].actions === 0) {
+        this.turnCounter--;
+        this.player[game.currentPlayer].actions++;
+
+      if (this.currentPlayer === this.player.length - 1) {
+          this.currentPlayer = 0;
+      } else {
+          this.currentPlayer++;
+        }
+      }
+  },
+  endRound(){
+    //const $rounds = $('#rounds');
+    this.turnCounter += 4;
+    this.rounds++;
+    currentRound();
+    //$rounds.text(`Round: ${this.rounds}`);
+
   },
   endGame(){
     if (this.rounds === 9) {
@@ -137,14 +165,13 @@ const game = {
     }
   }
 }
-
+// consider using e.target to design one reusable handler
 $('#red').on('click', (e) => {
   console.log($([e.target]).text())
-  if (game.turnCounter > 0) {
-    game.addRed();
-    game.addRed();
-    game.turnCounter--;
-    //user.showStats();DUCATION
+  if (game.player[game.currentPlayer].actions > 0) {
+    game.player[game.currentPlayer].addRed();
+
+
   } else {
       const $redButton = $(`#red`);
       $redButton.prop(`disabled`, true);
@@ -153,11 +180,10 @@ $('#red').on('click', (e) => {
 });
 
 $('#blue').on('click', () => {
-  if (game.turnCounter > 0) {
-    game.addBlue();
-    game.addBlue();
-    game.turnCounter--;
-  //  user.showStats();
+  if (game.player[game.currentPlayer].actions > 0) {
+    game.player[game.currentPlayer].addBlue();
+
+
   } else {
       const $blueButton = $(`#blue`);
       $blueButton.prop(`disabled`, true);
@@ -166,11 +192,10 @@ $('#blue').on('click', () => {
 });
 
 $('#green').on('click', () => {
-  if (game.turnCounter > 0) {
-    game.addGreen();
-    game.addGreen();
-    game.turnCounter--;
-    //user.showStats();
+  if (game.player[game.currentPlayer].actions > 0) {
+    game.player[game.currentPlayer].addGreen();
+
+
   } else {
       const $greenButton = $(`#green`);
       $greenButton.prop(`disabled`, true);
@@ -179,50 +204,40 @@ $('#green').on('click', () => {
 });
 
 $('#random').on('click', () => {
-  if (game.turnCounter > 0) {
-    game.addRandom();
-    game.addRandom();
-    game.addRandom();
-    game.turnCounter--;
-  //  user.showStats();
+  if (game.player[game.currentPlayer].actions > 0) {
+    game.player[game.currentPlayer].addRandom();
+    game.player[game.currentPlayer].addRandom();
+    game.player[game.currentPlayer].addRandom();
+    game.player[game.currentPlayer].actions--;
+
   } else {
       const $randomButton = $(`#random`);
       $randomButton.prop(`disabled`, true);
       console.log('No more actions available');
   }
-  console.log(game.player.red, " :red");
-  console.log(game.player.blue, " :blue");
-  console.log(game.player.green, " :green");
-});
-
-$('#buy').on('click', () => {
-  game.buyBasicBuilding();
-//  user.showStats();
-
-  console.log(game.player.buildings,' :event');
 
 });
+
+// $('#buy').on('click', () => {
+//   game.buyBasicBuilding();
+//   console.log(game.player.buildings,' :event');
+// });
 
 $('#endTurn').on('click', () => {
   game.endTurn();
+  game.currentTurn();
   game.buttonsDisable();
-
-
 });
+
 $('form').on('submit', (e) => {
-  // $('#inputBox').val();
   e.preventDefault();
   game.createPlayer();
-  // $('form').hide();
-  // $('form').hide();
-  // $('inputBox').html();
+
   // change attr of placeholder with interpolation of array index
-  //game.createPlayer();
-  // game.start();
+
 });
 $('#start').on('click', () => {
-
   $('form').hide();
-
+  game.currentTurn();
+  game.currentRound();
 });
-//game.start();
